f19335e9968d211f7f08fe9e9fae4163c937ea2d
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-05 12:09:49 -0700

    finsihed

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Classes
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            car1.Make = "Subaru";
+            car1.Model = "WRX";
+            car1.Year = 2021;
+
+            Console.WriteLine($"My car is {car1.Year} {car1.Make} {car1.Model}");
+
         }
     }
 }
